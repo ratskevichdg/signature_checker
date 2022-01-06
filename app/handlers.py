db9fac7761485f650ca1c0c5ed3c67302a243d88
Dzmitry Ratskevich <ratskevichdg@Dzmitrys-MacBook-Pro.local>
2022-01-06 18:20:41 +0300

    fix a bug where not all types of events were handled correctly

--- app/handlers.py
+++ app/handlers.py
@@ -2,10 +2,10 @@ import jwt
 import json
 import datetime
 import os
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 from starlette import status
+from typing import Optional
 
-from app.forms import Event
 from app.send_pubsub_msg import send_message_to_pub_sub_topic
 from app.slack_messaging import send_error_message
 
@@ -14,15 +14,23 @@ router = APIRouter()
 KEY = os.environ['KEY']
 
 @router.post('/server_event/')
-def get_items(*, appId: str, accountId: str, sessionId: str, signature: str, event: Event):
-    encoded_signature = jwt.encode(event.dict(), key=KEY, algorithm="HS512")
-    if encoded_signature == signature:
-        event_plus_timestamp = json.dumps({
-            "event_data:": event.dict(), 
-            "utc_timestamp": datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")
-        })
-        send_message_to_pub_sub_topic(event_plus_timestamp)
-        return {"status": "Event Successfully Sent"}
-    else:
-        send_error_message(session_id=sessionId, app_id=appId, account_id=accountId)
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Signatures didn't Match")
+async def get_items(
+        *,
+        appId: str, 
+        accountId: str, 
+        sessionId: Optional[str] = 'undefined', 
+        signature: str, 
+        event: Request
+    ):
+        request_body = await event.json()
+        encoded_signature = jwt.encode(request_body, key=KEY, algorithm="HS512")
+        if encoded_signature == signature:
+            event_plus_timestamp = json.dumps({
+                "event_data:": request_body, 
+                "utc_timestamp": datetime.datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S")
+            })
+            send_message_to_pub_sub_topic(event_plus_timestamp)
+            return {"status": "Event Successfully Sent"}
+        else:
+            send_error_message(session_id=sessionId, app_id=appId, account_id=accountId)
+            raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Signatures didn't Match")
